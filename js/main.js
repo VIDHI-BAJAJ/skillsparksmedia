9e1cde181424453011d6f7ba4307dc13218c1169
Vidhi Bajaj <vidhibajaj1401@gmail.com>
2025-11-25 14:43:38 +0530

    update

--- js/main.js
+++ js/main.js
@@ -1243,28 +1243,6 @@ PageInits = {
   },
 };
  // Add the testimonialsCarousel function here, right before the closing brace of PageInits:
-PageInits.testimonialsCarousel = function () {
-    const customersTestimonials = document.querySelector('#customers-testimonials');
-    
-    if (customersTestimonials && $(customersTestimonials).owlCarousel) {
-      $(customersTestimonials).owlCarousel({
-        loop: true,
-        center: true,
-        items: 3,
-        margin: 0,
-        autoplay: true,
-        dots: true,
-        autoplayTimeout: 8500,
-        smartSpeed: 450,
-        responsive: {
-          0: { items: 1 },
-          768: { items: 2 },
-          1170: { items: 3 }
-        }
-      });
-    }
-  }
-};
 
 
 function funZoneCanvasInit() {
